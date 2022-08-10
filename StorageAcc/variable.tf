61cebac1b0ff5a20001a1ddc636ff36aa16e6943
bvamsipriyavardhan <108339346+bvamsipriyavardhan@users.noreply.github.com>
2022-08-10 12:33:32 +0530

    variable

--- StorageAcc/variable.tf
+++ StorageAcc/variable.tf
@@ -27,7 +27,6 @@ variable "resource_group_name" {
     type = string
     description = "Name of the resource group"
 }
-
 variable "location" {
     type = string
     description = "The location for the deployment"
